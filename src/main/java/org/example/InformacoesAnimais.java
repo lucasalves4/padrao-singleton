754d89285872ebab7d5542aaf33f08996969b636
lucasalves4 <lucasalves.12@live.com>
2024-03-18 22:35:50 -0300

    alterar classes

--- src/main/java/org/example/InformacoesAnimais.java
+++ src/main/java/org/example/InformacoesAnimais.java
@@ -1,2 +1,56 @@
-package org.example;public class InformacoesAnimais {
+package org.example;
+
+public class InformacoesAnimais {
+
+    private InformacoesAnimais() {};
+    private static InformacoesAnimais instance = new InformacoesAnimais();
+    public static InformacoesAnimais getInstance() {
+        return instance;
+    }
+
+    private String nomeAnimal;
+    private String especie;
+    private String raca;
+    private int idade;
+    private String tutor;
+
+    public String getNomeAnimal() {
+        return nomeAnimal;
+    }
+
+    public void setNomeAnimal(String nomeAnimal) {
+        this.nomeAnimal = nomeAnimal;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getRaça() {
+        return raca;
+    }
+
+    public void setRaça(String raca) {
+        this.raca = raca;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(String tutor) {
+        this.tutor = tutor;
+    }
 }
--- src/test/java/org/example/InformacoesAnimaisTest.java
+++ src/test/java/org/example/InformacoesAnimaisTest.java
@@ -1,4 +1,21 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class InformacoesAnimaisTest {
-  
+
+    @Test
+    public void deveRetornarEspecieAnimal() {
+        InformacoesAnimais.getInstance().setEspecie("Gato");
+        assertEquals("Gato", InformacoesAnimais.getInstance().getEspecie());
+    }
+
+    @Test
+    public void deveRetornarNomeAnimal() {
+        InformacoesAnimais.getInstance().setNomeAnimal("Banguela");
+        assertEquals("Banguela", InformacoesAnimais.getInstance().getNomeAnimal());
+    }
+
 }
